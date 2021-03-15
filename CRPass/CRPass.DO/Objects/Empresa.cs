284c78b1d79f5402c6311f637358bf957f0e26c1
LorianAg <loriana.aguilar02@gmail.com>
2021-03-15 00:13:28 -0600

    Creacion de las clases en DO.Objects

---
+++ CRPass/CRPass.DO/Objects/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRPass.DO.Objects
+{
+    public partial class Empresa
+    {
+        public int CodEmpresa { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public int ReservasUsuario { get; set; }
+
+        //public virtual ICollection<Boleteria> Boleteria { get; set; }
+        //public virtual ICollection<ControlAforo> ControlAforo { get; set; }
+        //public virtual ICollection<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ CRPass/CRPass.DO/Objects/Tickets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRPass.DO.Objects
+{
+    public partial class Tickets
+    {
+        public int CodTicket { get; set; }
+        public string Nreserva { get; set; }
+        public string Usuario { get; set; }
+        public int CodEmpresa { get; set; }
+        public DateTime Fecha { get; set; }
+        public int? Estado { get; set; }
+    }
+}
