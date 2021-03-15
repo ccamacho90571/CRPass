6dcc39642423cd2f9b3d3c62e1bbaf1d51b4b573
LorianAg <loriana.aguilar02@gmail.com>
2021-03-15 00:17:05 -0600

    Creacion clases BS (Tickets, EMpresas)

---
+++ CRPass/CRPass.BS/Empresa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CRPass.DAL.EF;
+using CRPass.DO.Interfaces;
+using data = CRPass.DO.Objects;
+
+namespace CRPass.BS
+{
+    public class Empresa : ICRUD<data.Empresa>
+    {
+        private SolutionDbContext context;
+        public Empresa(SolutionDbContext _context)
+        {
+            context = _context;
+        }
+
+        public void Delete(data.Empresa t)
+        {
+            new DAL.Empresa(context).Delete(t);
+        }
+
+        public IEnumerable<data.Empresa> GetAll()
+        {
+            return new DAL.Empresa(context).GetAll();
+        }
+
+        public data.Empresa GetOneById(int id)
+        {
+            return new DAL.Empresa(context).GetOneById(id);
+        }
+
+        public void Insert(data.Empresa t)
+        {
+            new DAL.Empresa(context).Insert(t);
+        }
+
+        public void Update(data.Empresa t)
+        {
+            new DAL.Empresa(context).Update(t);
+        }
+    }
+}
---
+++ CRPass/CRPass.BS/Tickets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CRPass.DAL.EF;
+using CRPass.DO.Interfaces;
+using data = CRPass.DO.Objects;
+
+namespace CRPass.BS
+{
+    public class Tickets : ICRUD<data.Tickets>
+
+    {
+        private SolutionDbContext context;
+        public Tickets(SolutionDbContext _context)
+        {
+            context = _context;
+        }
+
+        public void Delete(data.Tickets t)
+        {
+            new DAL.Tickets(context).Delete(t);
+        }
+
+        public IEnumerable<data.Tickets> GetAll()
+        {
+            return new DAL.Tickets(context).GetAll();
+        }
+
+        public data.Tickets GetOneById(int id)
+        {
+            return new DAL.Tickets(context).GetOneById(id);
+        }
+
+        public void Insert(data.Tickets t)
+        {
+            new DAL.Tickets(context).Insert(t);
+        }
+
+        public void Update(data.Tickets t)
+        {
+            new DAL.Tickets(context).Update(t);
+        }
+    }
+}
