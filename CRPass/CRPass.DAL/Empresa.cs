38ba8ebed5aaf40fa64668804fb6b3a5c3aef399
LorianAg <loriana.aguilar02@gmail.com>
2021-03-15 00:16:30 -0600

    Creacion clases DAL (Tickets y empresas)

---
+++ CRPass/CRPass.DAL/Empresa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data = CRPass.DO.Objects;
+using CRPass.DAL.EF;
+using CRPass.DO.Interfaces;
+using CRPass.DAL.Repository;
+
+namespace CRPass.DAL
+{
+    public class Empresa : ICRUD<data.Empresa>
+    {
+        private Repository<data.Empresa> _repo = null;
+        public Empresa(SolutionDbContext solutionDbContext)
+        {
+            _repo = new Repository<data.Empresa>(solutionDbContext);
+        }
+
+        public void Delete(data.Empresa t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.Empresa> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public data.Empresa GetOneById(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public void Insert(data.Empresa t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.Empresa t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
+    }
+}
---
+++ CRPass/CRPass.DAL/Tickets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data = CRPass.DO.Objects;
+using CRPass.DAL.EF;
+using CRPass.DO.Interfaces;
+using CRPass.DAL.Repository;
+
+namespace CRPass.DAL
+{
+    public class Tickets : ICRUD<data.Tickets>
+    {
+        private Repository<data.Tickets> _repo = null;
+        public Tickets(SolutionDbContext solutionDbContext)
+        {
+            _repo = new Repository<data.Tickets>(solutionDbContext);
+        }
+
+        public void Delete(data.Tickets t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.Tickets> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public data.Tickets GetOneById(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public void Insert(data.Tickets t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.Tickets t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
+    }
+}
