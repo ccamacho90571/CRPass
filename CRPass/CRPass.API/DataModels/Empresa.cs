ce14d030a6f36ec2954384f94a223e9ddd0f6810
LorianAg <loriana.aguilar02@gmail.com>
2021-03-15 00:18:11 -0600

    Creacion clases de Datamodels

---
+++ CRPass/CRPass.API/DataModels/Empresa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRPass.API.DataModels
+{
+    public class Empresa
+    {
+        public int CodEmpresa { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public int ReservasUsuario { get; set; }
+    }
+}
---
+++ CRPass/CRPass.API/DataModels/Tickets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRPass.API.DataModels
+{
+    public class Tickets
+    {
+        public int CodTicket { get; set; }
+        public string Nreserva { get; set; }
+        public string Usuario { get; set; }
+        public int CodEmpresa { get; set; }
+        public DateTime Fecha { get; set; }
+        public int? Estado { get; set; }
+    }
+}
