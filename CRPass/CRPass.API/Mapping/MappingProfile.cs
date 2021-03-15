8a247b04f1385a0f57683bcea4d6bca744f518eb
LorianAg <loriana.aguilar02@gmail.com>
2021-03-15 00:17:55 -0600

    Creacion carpeta Mapping y clase MappingProfile

---
+++ CRPass/CRPass.API/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using data = CRPass.DO.Objects;
+
+namespace CRPass.API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            // Representa el casting del objeto a el similar 
+            // Desde DO hacia DataModels 
+            // Domain to Resource
+
+            CreateMap<data.Empresa, DataModels.Empresa>().ReverseMap();
+
+            CreateMap<data.Tickets, DataModels.Tickets>().ReverseMap();
+
+        }
+    }
+}
