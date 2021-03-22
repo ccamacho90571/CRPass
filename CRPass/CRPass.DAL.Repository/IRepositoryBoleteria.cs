f325512254f6d74775919a7822b40c711c076437
LorianAg <loriana.aguilar02@gmail.com>
2021-03-21 22:49:40 -0600

    Creacion IRepositoryBoleteria y clase de Boleteria en DAL.Repository

---
+++ CRPass/CRPass.DAL.Repository/IRepositoryBoleteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data = CRPass.DO.Objects;
+using System.Threading.Tasks;
+
+namespace CRPass.DAL.Repository
+{
+    public interface IRepositoryBoleteria: IRepository<data.Boleteria>
+    {
+        Task<IEnumerable<data.Boleteria>> GetAllWithAsAsync();
+
+        Task<data.Boleteria> GetByOneWithAsAsync(int id);
+    }
+}
---
+++ CRPass/CRPass.DAL.Repository/RepositoryBoleteria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data = CRPass.DO.Objects;
+using System.Threading.Tasks;
+using CRPass.DAL.EF;
+using CRPass.DO.Objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRPass.DAL.Repository
+{
+    public class RepositoryBoleteria : Repository<data.Boleteria>, IRepositoryBoleteria
+    {
+        public RepositoryBoleteria(SolutionDbContext context)
+           : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Boleteria>> GetAllWithAsAsync()
+        {
+            return await _db.Boleteria
+                .Include(m => m.CodEmpresaNavigation)
+                .ToListAsync();
+        }
+
+        public async Task<Boleteria> GetByOneWithAsAsync(int id)
+        {
+            return await _db.Boleteria
+             .Include(m => m.CodEmpresaNavigation)
+             .SingleOrDefaultAsync(m => m.CodEmpresa == id);
+        }
+
+        private SolutionDbContext _db
+        {
+            get { return dbContext as SolutionDbContext; }
+        }
+    }
+}
