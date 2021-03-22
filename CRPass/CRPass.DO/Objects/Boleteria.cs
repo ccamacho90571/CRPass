752786ca00ed496b5d75e8bbe7dc82a295a6e664
LorianAg <loriana.aguilar02@gmail.com>
2021-03-21 22:48:44 -0600

    Creacion DO OBjects Boleteria

---
+++ CRPass/CRPass.DO/Objects/Boleteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRPass.DO.Objects
+{
+    public partial class Boleteria
+    {
+        public int CodBoleteria { get; set; }
+        public int CodEmpresa { get; set; }
+        public string Descripcion { get; set; }
+        public int Costo { get; set; }
+
+        public virtual Empresa CodEmpresaNavigation { get; set; }
+        //public virtual ICollection<BoleteriaReservados> BoleteriaReservados { get; set; }
+    }
+}
