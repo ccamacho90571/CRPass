a5507787db7131aaa467cd0c9f6446d5e2f4e6b5
LorianAg <loriana.aguilar02@gmail.com>
2021-03-21 22:50:39 -0600

    Creacion clase Boleteria en datamodels

---
+++ CRPass/CRPass.API/DataModels/Boleteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRPass.API.DataModels
+{
+    public class Boleteria
+    {
+        public int CodBoleteria { get; set; }
+        public int CodEmpresa { get; set; }
+        public string Descripcion { get; set; }
+        public int Costo { get; set; }
+    }
+}
