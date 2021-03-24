fc2870ae600e6a84f8be2977669869f7b9311749
LorianAg <loriana.aguilar02@gmail.com>
2021-03-23 21:58:55 -0600

    Creacion clase Datamodels BoleteriaReservados

---
+++ CRPass/CRPass.API/DataModels/BoleteriaReservados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRPass.API.DataModels
+{
+    public class BoleteriaReservados
+    {
+        public int CodBoletaReservado { get; set; }
+        public int CodBoleteria { get; set; }
+        public int CodTickets { get; set; }
+        public int? Cantidad { get; set; }
+    }
+}
