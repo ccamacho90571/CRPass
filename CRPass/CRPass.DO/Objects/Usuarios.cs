aaa64af4027dd61e45d420e51bc127513c625223
Carlos Camacho <ccamacho90571@ufide.ac.cr>
2021-03-29 21:59:13 -0600

    Cambios usuarios y aforo

---
+++ CRPass/CRPass.DO/Objects/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRPass.DO.Objects
+{
+    class Usuarios
+    {
+
+        public int CodUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
