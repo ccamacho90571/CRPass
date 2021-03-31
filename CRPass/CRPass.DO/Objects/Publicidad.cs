2728eb5100bfc0e325c6b452c8e66860b97eb69d
LorianAg <loriana.aguilar02@gmail.com>
2021-03-30 21:19:04 -0600

    Creacion clase DO publicidad

---
+++ CRPass/CRPass.DO/Objects/Publicidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRPass.DO.Objects
+{
+    public partial class Publicidad
+    {
+        public int CodPublicidad { get; set; }
+        public int CodEmpresa { get; set; }
+        public string RutaArchivo { get; set; }
+
+        public virtual Empresa CodEmpresaNavigation { get; set; }
+    }
+}
