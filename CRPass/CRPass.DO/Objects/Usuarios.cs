9345215d8770ab935eee803c91cd0213ef18c2fb
LorianAg <loriana.aguilar02@gmail.com>
2021-04-11 00:09:19 -0600

    Modificacion clase Usuarios DO

--- CRPass/CRPass.DO/Objects/Usuarios.cs
+++ CRPass/CRPass.DO/Objects/Usuarios.cs
@@ -7,12 +7,13 @@ namespace CRPass.DO.Objects
     public class Usuarios
     {
 
-        public int CodUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public bool Tipo { get; set; }
+        public int CodEmpresa { get; set; }
         public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Cedula { get; set; }
-        public string Telefono { get; set; }
         public string Correo { get; set; }
+
         public virtual Empresa CodEmpresaNavigation { get; set; }
         public virtual ICollection<Tickets> Tickets { get; set; }
 
