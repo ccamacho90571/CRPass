928c82b2e24757ac642cdf3e08b0e3b886cf182e
LorianAg <loriana.aguilar02@gmail.com>
2021-04-11 00:09:30 -0600

    Modificacion clase Tickets DO

--- CRPass/CRPass.DO/Objects/Tickets.cs
+++ CRPass/CRPass.DO/Objects/Tickets.cs
@@ -12,7 +12,6 @@ namespace CRPass.DO.Objects
         public int CodEmpresa { get; set; }
         public DateTime Fecha { get; set; }
         public int? Estado { get; set; }
-
         public virtual Empresa CodEmpresaNavigation { get; set; }
         public virtual Usuarios UsuarioNavigation { get; set; }
         public virtual ICollection<BoleteriaReservados> BoleteriaReservados { get; set; }
